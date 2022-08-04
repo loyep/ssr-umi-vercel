1282a2aea9cc35bff984c33f276ae2f052fd376d
Zack <lorenx@163.com>
2022-08-04 15:21:32 +0800

    feat: 更新

--- src/.umi-production/umi.ts
+++ src/.umi-production/umi.ts
@@ -3,7 +3,7 @@
 // DO NOT CHANGE IT MANUALLY!
 import './core/polyfill';
 
-import { renderClient } from '/Users/heixing/Git/umi/packages/renderer-react';
+import { renderClient } from '/Users/heixing/Git/ditejs/ssr-umi/node_modules/@umijs/renderer-react';
 import { getRoutes } from './core/route';
 import { createPluginManager } from './core/plugin';
 import { createHistory } from './core/history';
@@ -37,9 +37,8 @@ async function render() {
         initialValue: {},
       });
       const basename = contextOpts.basename || '/';
-      console.log('basename', basename);
       const context = {
-        hydrate: true,
+    hydrate: true,
         routes,
         routeComponents,
         pluginManager,
